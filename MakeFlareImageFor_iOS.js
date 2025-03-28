e0f413f075bcd79b5a8f034a85c079c789e07847
Maruhati3 <89867660+Maruhati3@users.noreply.github.com>
2025-03-29 01:58:50 +0900

    微修正

--- MakeFlareImageFor_iOS.js
+++ MakeFlareImageFor_iOS.js
@@ -87,15 +87,55 @@ window.MakeFlareImage = function () {
   document.querySelector(".main").children[2].remove();
 
 
-  Pname1="Dancer Name: ";
-  Pname2=document.querySelectorAll(".name_str")[1];
-  TotalFlare1="Total Flare Skill";
-  TotalFlare2=parseInt(sumC)+parseInt(sumW)+parseInt(sumG);
-  document.querySelector(".flareskill_classic_head").textContent+=  ("Classic Sum: " +sumC+"Classic average : "+(sumC/30).toPrecision(4));
-  document.querySelector(".flareskill_white_head").textContent+=    ("White   Sum: "   +sumW+"White   average : "+(sumC/30).toPrecision(4));
-  document.querySelector(".flareskill_gold_head").textContent+=     ("Gold    Sum: "    +sumG+"Gold    average : "+(sumC/30).toPrecision(4));
+  Pname1='<span style="font-size:30px">Dancer Name&nbsp;</span>';
+  Pname2=document.querySelectorAll(".name_str")[1].innerHTML;
+  TotalFlare1='<span style="font-size:30px">Total Flare Skill &nbsp;</span>';
+  TotalFlare2=(parseInt(sumC)+parseInt(sumW)+parseInt(sumG));
+  
+  sumC=     ("Classic&nbsp;Sum: " +sumC+"&nbsp;&nbsp;Classic average : "+(sumC/30).toPrecision(4));
+  sumW=     ("White&nbsp;&nbsp;&nbsp;Sum: "     +sumW  +"&nbsp;&nbsp;White&nbsp;&nbsp;&nbsp;average : "+(sumW/30).toPrecision(4));
+  sumG=     ("Gold&nbsp;&nbsp;&nbsp;&nbsp;Sum: "+sumG +"&nbsp;&nbsp;Gold&nbsp;&nbsp;&nbsp;&nbsp;average : "+(sumG/30).toPrecision(4));
+
+  let PlayerData = document.createElement("div");
+  PlayerData.id="PlayerData";
+  PlayerData.style="font-weight:600;width:100%;height:180px;display:flex;flex-wrap: wrap;justify-content: flex-around;align-items:flex-center;align-content:space-around;"
+  document.querySelector(".main").insertBefore(PlayerData,document.querySelector(".game_inner"))
+  let PlayerDataChild1=document.createElement("div");
+  PlayerDataChild1.id="PlayerData1";
+  PlayerDataChild1.style="width:35%;height:100%;font-size:50px;";
+  document.querySelector("#PlayerData").appendChild(PlayerDataChild1);
+  let PlayerDataChild2=document.createElement("div");
+  PlayerDataChild2.id="PlayerData2";
+  PlayerDataChild2.style="width:65%;height:100%;font-size:40px;";
+  document.querySelector("#PlayerData").appendChild(PlayerDataChild2);
+  
+  let PDC1C1=document.createElement("p");
+  PDC1C1.style="width:100%;height:50%;text-align:center;vertical-align: middle;display:flex;justify-content:center;align-items:center;";
+  PDC1C1.innerHTML=Pname1+Pname2;
+  let PDC1C2=document.createElement("p");
+  PDC1C2.style="width:100%;height:50%;text-align:center;vertical-align: middle;display:flex;justify-content:center;align-items:center;";
+  PDC1C2.innerHTML = TotalFlare1 + TotalFlare2;
+
+  document.querySelector("#PlayerData1").appendChild(PDC1C1);
+  document.querySelector("#PlayerData1").appendChild(PDC1C2);
+
+  let PDC2C1=document.createElement("p");
+  PDC2C1.style="background:linear-gradient(90deg,  #cd9370, #c1703e, #cd9370);-webkit-background-clip: text; -webkit-text-fill-color: transparent;width: 100%; height: calc(33.3333%); text-align: center; vertical-align: middle; display: flex; justify-content: center; align-items: center;"
+  PDC2C1.innerHTML = sumC;
+  let PDC2C2=document.createElement("p");
+  PDC2C2.style="background:linear-gradient(90deg,#cfcfce, #b9b7b7);-webkit-background-clip: text; -webkit-text-fill-color: transparent;width: 100%; height: calc(33.3333%); text-align: center; vertical-align: middle; display: flex; justify-content: center; align-items: center;"
+  PDC2C2.innerHTML = sumW;
+  let PDC2C3=document.createElement("p");
+  PDC2C3.style="background:linear-gradient(90deg,  #ffdd2e, #ffea49, #995e1d, #723a22, #b67f18, #f0c41a, #c69f37, #936723);;-webkit-background-clip: text; -webkit-text-fill-color: transparent;width: 100%; height: calc(33.3333%); text-align: center; vertical-align: middle; display: flex; justify-content: center; align-items: center;"
+  PDC2C3.innerHTML = sumG;
+
+  
+  document.querySelector("#PlayerData2").appendChild(PDC2C1);
+  document.querySelector("#PlayerData2").appendChild(PDC2C2);
+  document.querySelector("#PlayerData2").appendChild(PDC2C3);
 
   document.querySelector(".main").style = "width:1400px;";
+  
 
   let loader = document.createElement("div");
   loader.id = "loader";
